8f10f6e417a8cd31e9477fbbb44051f6fca3f489
github username <samson.yuwono@gmail.com>
2019-02-13 11:24:31 -0500

    refactored for submission

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 let value;
 let page = 1;
-function assignValue(event) {
+
+function assignSearchValue(event) {
   value = event.target.value;
   document.getElementById("search").value = event.target.value;
 }
 
+function showResults() {
+  showPagination();
+  fetchImages();
+}
+
 function fetchImages() {
-  fetch(
-    `${secrets.BASE_URL}${secrets.API_KEY}&tags=${value}${
-      secrets.FORMAT_URL
-    }${page}`
-  )
-    .then(res => res.json())
-    .then(json => {
-      renderPhotos(json.photos.photo);
-    });
+  if (value.length > 0) {
+    fetch(
+      `${secrets.BASE_URL}${secrets.API_KEY}&tags=${value}${
+        secrets.FORMAT_URL
+      }${page}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        renderPhotos(json.photos.photo);
+      });
+  } else {
+    alert("Please enter a value");
+  }
 }
 
 function renderPhotos(photos) {
-  let images = `${photos.map(
+  let images = formatResults(photos);
+  images = removeCommas(images);
+  document.getElementById("output").innerHTML = images;
+}
+
+function removeCommas(images) {
+  return images.replace(/,/g, "");
+}
+
+function formatResults(photos) {
+  return `${photos.map(
     photo =>
       '<div class="photo-container"><img onclick="displayModal(event)" src=' +
       "http://farm" +
@@ -32,27 +52,27 @@ function renderPhotos(photos) {
       ".jpg>" +
       "</img></div>"
   )}`;
-
-  images = images.replace(/,/g, "");
-  document.getElementById("output").innerHTML = images;
 }
 
-function hidePaginate() {}
-
-function nextPage(next) {
-  if (next) {
-    page++;
-  }
+function pagination(next) {
+  modifyPageNumber(next);
   fetchImages();
 }
+function showPagination() {
+  if (page >= 1) {
+    document.getElementById("pagination").style.display = "block";
+  }
+}
 
-function previousPage() {
-  if (page > 1) {
+function modifyPageNumber(next) {
+  if (next) {
+    page++;
+  } else if (page > 1) {
     page--;
   } else {
     page;
   }
-  fetchImages();
+  return page;
 }
 
 function displayModal(event) {
